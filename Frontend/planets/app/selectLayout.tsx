2c325c840db1a689224f05854955249795852eae
DorinaSobiecki <dorinasob@gmail.com>
2024-05-08 09:30:45 +0200

    [#8] feat: created border svg

--- Frontend/planets/app/selectLayout.tsx
+++ Frontend/planets/app/selectLayout.tsx
@@ -1,24 +1,26 @@
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 import "./globals.css"
 import "./selectLayout.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
-import React, { ReactNode } from 'react';
+import React, {ReactNode} from 'react';
 
 interface SelectLayoutProps {
     leftChildren: ReactNode;
     rightChildren: ReactNode;
 }
 
-const SelectLayout: React.FC<SelectLayoutProps> = ({ leftChildren, rightChildren }) => {
+const SelectLayout: React.FC<SelectLayoutProps> = ({leftChildren, rightChildren}) => {
     return (
         <div className={`${inter.className} min-h-screen flex justify-between p-20 bg-star`}>
+
             <div className="w-2/5 mr-28 border--black--yellow">
                 <div className="inner-box">
-                {leftChildren}
+                    {leftChildren}
                 </div>
             </div>
+
             <div className="w-3/5 border--black--yellow">
                 <div className="inner-box">
                     {rightChildren}
