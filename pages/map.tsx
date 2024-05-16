74e9e292418ff5c07ffc5ab3ecbd3591a4a720df
ds217 <ds217@hdm-stuttgart.de>
2024-05-16 17:30:32 +0200

    [#26] feat: created map page

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './map.css';
 import Layout from '../app/layout';
 import Image from "next/image";
@@ -56,52 +56,54 @@ const MapPage: React.FC = () => {
     return (
         <Layout>
             <div className={"hide-scrollbar overflow-auto"}>
-            <div ref={orbitContainerRef} className="bg-star h-screen hide-scrollbar relative overflow-y-hidden overflow-x-auto">
-                {['Neptune', 'Uranus', 'Saturn', 'Jupiter', 'Mars', 'Erde', 'Venus', 'Merkur'].map((planet) => (
-                    <div className={`orbit orbit--${planet.toLowerCase()}`} key={planet}>
-                        <div className="planet">
-                            <Image
-                                /* We use unoptimized because the optimized version causes problems for gifs */
-                                unoptimized={true}
-                                src={getImageSrc(planet, hoveredPlanet === planet, selectedPlanet === planet)}
-                                alt={planet}
-                                width={100}
-                                height={100}
-                                onClick={() => planetClick(planet)}
-                                onMouseEnter={() => handleMouseEnter(planet)}
-                                onMouseLeave={handleMouseLeave}
-                            />
-                            <div
-                                className={`planet__name ${selectedPlanet === planet ? 'planet__name--selected' : ''} ${planetCompletion[planet] ? 'planet__name--completed' : ''}`}
-                                onClick={() => planetClick(planet)}
-                                onMouseEnter={() => handleMouseEnter(planet)}
-                                onMouseLeave={handleMouseLeave}
-                            >
-                                {planet}
+                <div ref={orbitContainerRef}
+                     className="bg-star h-screen hide-scrollbar relative overflow-y-hidden overflow-x-auto">
+                    {['Neptune', 'Uranus', 'Saturn', 'Jupiter', 'Mars', 'Erde', 'Venus', 'Merkur'].map((planet) => (
+                        <div className={`orbit absolute rounded-full orbit--${planet.toLowerCase()}`} key={planet}>
+                            <div className="planet absolute flex flex-col align-middle gap-4 z-50">
+                                <Image
+                                    /* We use unoptimized because the optimized version causes problems for gifs */
+                                    unoptimized={true}
+                                    className={"hover:cursor-pointer"}
+                                    src={getImageSrc(planet, hoveredPlanet === planet, selectedPlanet === planet)}
+                                    alt={planet}
+                                    width={100}
+                                    height={100}
+                                    onClick={() => planetClick(planet)}
+                                    onMouseEnter={() => handleMouseEnter(planet)}
+                                    onMouseLeave={handleMouseLeave}
+                                />
+                                <div
+                                    className={`hover:cursor-pointer planet__name ${selectedPlanet === planet ? 'planet__name--selected' : ''} ${planetCompletion[planet] ? 'planet__name--completed' : ''}`}
+                                    onClick={() => planetClick(planet)}
+                                    onMouseEnter={() => handleMouseEnter(planet)}
+                                    onMouseLeave={handleMouseLeave}
+                                >
+                                    {planet}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-                <div className={"planet"}>
-                    <Image
-                        priority={true}
-                        className="planet--sun absolute -left-60 hover:cursor-pointer"
-                        src={getImageSrc('Sun', hoveredPlanet === 'Sun', selectedPlanet === 'Sun')}
-                        alt="Sonne"
-                        width={450}
-                        height={450}
-                        onClick={() => planetClick('Sun')}
-                        onMouseEnter={() => handleMouseEnter('Sun')}
-                        onMouseLeave={handleMouseLeave}
-                    />
-                    <div
-                        className={`planet__name--sun planet__name ${selectedPlanet === 'Sun' ? 'planet__name--selected' : ''} ${planetCompletion.Sun ? 'planet__name--completed' : ''}`}
-                        onClick={() => planetClick('Sun')}
-                    >
-                        Sonne
+                    ))}
+                    <div className={"planet"}>
+                        <Image
+                            priority={true}
+                            className="planet--sun  max-w-none absolute -left-56 hover:cursor-pointer"
+                            src={getImageSrc('Sun', hoveredPlanet === 'Sun', selectedPlanet === 'Sun')}
+                            alt="Sonne"
+                            width={450}
+                            height={450}
+                            onClick={() => planetClick('Sun')}
+                            onMouseEnter={() => handleMouseEnter('Sun')}
+                            onMouseLeave={handleMouseLeave}
+                        />
+                        <div
+                            className={`planet__name--sun left-4 !absolute planet__name ${selectedPlanet === 'Sun' ? 'planet__name--selected' : ''} ${planetCompletion.Sun ? 'planet__name--completed' : ''}`}
+                            onClick={() => planetClick('Sun')}
+                        >
+                            Sonne
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </Layout>
     );
