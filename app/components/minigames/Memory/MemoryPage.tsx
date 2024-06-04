8fc906a87ce4a07f221dc236a1456c6eced6a868
SebastianMaier03 <Seb-Maier@outlook.de>
2024-06-04 15:50:29 +0200

    [#50] fix page size

--- app/components/minigames/Memory/MemoryPage.tsx
+++ app/components/minigames/Memory/MemoryPage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const MemoryPage: React.FC = () => {
     return (
-        <div className="flex flex-col justify-center items-center min-h-screen bg-star p-4">
+        <div className="flex flex-col justify-center items-center page-container bg-star p-4">
             <div className="text-center mb-4">
                 <h1 className="text-3xl font-bold mb-16 h1">Venus Entdeckungen: Ein Gedächtnisspiel</h1>
                 <h2 className="text-xl font-medium mb-8 h2">Decke abwechselnd zwei Karten auf und sammle Paare von passenden Bildern, um mehr über die Venus zu lernen.</h2>
