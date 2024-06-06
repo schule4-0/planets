aa348d3f20b53fe708d174035c76663dfcdd7921
ds217 <ds217@hdm-stuttgart.de>
2024-06-06 12:13:23 +0200

    [#50] fix: popup image position

--- app/components/minigames/Memory/PopupWindow.tsx
+++ app/components/minigames/Memory/PopupWindow.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, {FC} from "react";
 import Image from 'next/image';
 import ActionButton from "@/app/components/actionButton/ActionButton";
 import "./Popup.css"
@@ -9,16 +9,18 @@ type PopupWindowProps = {
     onClose: () => void;
 };
 
-const PopupWindow: FC<PopupWindowProps> = ({ content, imageUrl, onClose }) => {
+const PopupWindow: FC<PopupWindowProps> = ({content, imageUrl, onClose}) => {
     return (
         <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
-            <div className="text-black popup-box bg-white rounded-lg p-8 max-w-xl relative">
-                <h2 className="mb-16 h2">Richtig!</h2>
-                <div className="flex justify-between items-center">
-                    <p className="mb-4">{content}</p>
-                    <Image src={imageUrl} alt="Popup Image" width={150} height={150} className="ml-4"/>
+            <div className="text-black popup-box bg-white rounded-lg p-14 m-4 max-w-xl relative">
+                <div className="flex items-center">
+                    <div>
+                        <h2 className="mb-10">Richtig!</h2>
+                        <p>{content}</p>
+                    </div>
+                    <Image src={imageUrl} alt="Popup Image" width={150} height={150} className="ml-12"/>
                 </div>
-                    <ActionButton onClick={onClose} />
+                <ActionButton onClick={onClose}/>
             </div>
         </div>
     );
