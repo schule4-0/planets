7a7a187399f892e3d854db47084adfe40f9fbb48
SebastianMaier03 <Seb-Maier@outlook.de>
2024-06-15 00:18:15 +0200

    Revert "[28] fixt: deploy"
    This reverts commit e0bfb88ba4b9e986e706541a4ea0e389b4cb2324.

--- pages/spaceship.tsx
+++ pages/spaceship.tsx
@@ -14,6 +14,7 @@ import {
 import {getRocketColorIndex, getRocketColors} from '@/app/utils/colorUtils';
 
 const Spaceship: React.FC = () => {
+
     const isClient = typeof window !== 'undefined';
     const [selectedColor, setSelectedRocketColor] = useState<string>('orange');
     const [selectedRocketType, setSelectedRocketType] = useState<string>('rocket1');
