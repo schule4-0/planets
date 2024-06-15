74466273c6ac2d538d3174cbc130586a062acbba
ds217 <ds217@hdm-stuttgart.de>
2024-06-15 14:57:43 +0200

    [#100] feat: added dynamic spaceship part pick up

--- pages/collected-mercury.tsx
+++ pages/collected-mercury.tsx
@@ -5,7 +5,7 @@ const CollectedMars: React.FC = () => {
 
     return (
         <div className="page-container bg-star">
-            <CollectedSpaceshipPart imgSrc={"/images/raumschiff_bottom.png"} planet={"MARS"} nextPage={"/map"}/>
+            <CollectedSpaceshipPart imgSrc={"/images/raumschiff_middle2.png"} planet={"MERCURY"} nextPage={"/map"}/>
         </div>
     );
 };
