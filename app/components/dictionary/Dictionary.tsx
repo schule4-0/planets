78e3e084a76a1dfe4da2893f8bd577479d3e7452
DorinaSobiecki <dorinasob@gmail.com>
2024-07-02 23:28:32 +0200

    [#151] feat: reset selected letter when click again

--- app/components/dictionary/Dictionary.tsx
+++ app/components/dictionary/Dictionary.tsx
@@ -40,19 +40,24 @@ const Dictionary: React.FC = () => {
         return (
             <div className="fixed w-full flex justify-center items-center page-container bg-black bg-opacity-50 z-50">
                 <div className="w-5/6 h-5/6 bg-white text-black rounded-2xl p-6">
-                    <div className="h-1/6 mb-6">
+                    <div className="h-2/6">
                         <div className="flex justify-between">
                             <h1>Lexikon</h1>
                             <Image src="/images/close.png" alt="schließen" width={60} height={60}
-                                   onClick={() => router.push(router.pathname)}/>
+                                   onClick={() => router.push(router.pathname)} className={"cursor-pointer"}/>
                         </div>
                         <div className="text-center">
                             {alphabet.map(letter => (
                                 <button
                                     key={letter}
                                     onClick={() => {
-                                        setSelectedLetter(letter);
-                                        router.push(`?dictionary=${letter}`, undefined, {shallow: true});
+                                        if (selectedLetter === letter) {
+                                            setSelectedLetter('');
+                                            router.push(`${router.pathname}?dictionary`, undefined, { shallow: true });
+                                        } else {
+                                            setSelectedLetter(letter);
+                                            router.push(`?dictionary=${letter}`, undefined, { shallow: true });
+                                        }
                                     }}
                                     className={`subtitles !font-bold mx-4`}
                                     style={{color: selectedLetter === letter ? 'var(--active-color)' : 'black'}}
@@ -62,11 +67,11 @@ const Dictionary: React.FC = () => {
                             ))}
                         </div>
                     </div>
-                    <div className="overflow-y-auto h-5/6">
+                    <div className="h-4/6 overflow-y-auto">
                         {filteredItems.map((item, index) => (
-                            <div key={index} className="my-4 flex items-start gap-3">
+                            <div key={index} className="mb-9 flex items-start gap-3">
                                 <Image src={item.image} alt={item.header} width={200} height={200}
-                                       style={{minWidth: '200px', height: 'auto'}}/>
+                                       style={{minWidth: '200px', height: 'auto'}} className={"pt-2"}/>
                                 <div>
                                     <p className="subtitles">{item.header}</p>
                                     <h2>{item.info}</h2>
